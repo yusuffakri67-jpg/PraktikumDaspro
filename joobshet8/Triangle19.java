3c569e4999a889df0b0cfd320884d823961591a4
Fakri <yusuffakri67@example>
2025-11-06 00:24:07 +0700

    Percobaan 3

---
+++ joobshet8/Triangle19.java
@@ -0,0 +1,21 @@
+package PraktikumDaspro.joobshet8;
+
+import java.util.Scanner;
+public class Triangle19 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("masukkan nilai n = ");
+        int N = sc.nextInt();
+        int i = 0;
+        while (i < N) {
+            int j = 0;
+            while (j <= i) {
+                System.out.print("*");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+}
