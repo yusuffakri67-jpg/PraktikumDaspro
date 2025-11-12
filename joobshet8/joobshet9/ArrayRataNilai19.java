44f3153396e2b89e6d8b5e7a35b51663629684ae
Fakri <yusuffakri67@example>
2025-11-12 21:05:11 +0700

    percobaan 3

---
+++ joobshet8/joobshet9/ArrayRataNilai19.java
@@ -0,0 +1,25 @@
+package joobshet8.joobshet9;
+
+import java.util.Scanner;
+
+public class ArrayRataNilai19 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] nilaiMhs = new int[10];
+        double total = 0;
+        double rata2;
+
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            System.out.print("masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
+            nilaiMhs[i] = sc.nextInt();
+        }
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            total += nilaiMhs[i];
+        }
+
+        rata2 = total / nilaiMhs.length;
+        System.out.println(" Rata-Rata nilai = " + rata2);
+    }
+}
