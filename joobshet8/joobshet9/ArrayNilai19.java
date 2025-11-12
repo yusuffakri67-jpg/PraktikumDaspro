e4b10baf3c70aa9eb56022cd4df5c0a8a4f114e3
Fakri <yusuffakri67@example>
2025-11-12 20:39:17 +0700

    Percobaan 2

--- joobshet8/joobshet9/ArrayNilai19.java
+++ joobshet8/joobshet9/ArrayNilai19.java
@@ -1,5 +1,17 @@
 package joobshet8.joobshet9;
-import 
+import java.util.Scanner;
 public class ArrayNilai19 {
-    
+    public static void main(String[] args) {
+     Scanner sc = new Scanner(System.in);
+
+        int[] nilaiAkhir = new int [10];
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Masukkan nilai akhir ke-" + i + ": ");
+            nilaiAkhir[i] = sc.nextInt();
+        }
+        for (int i = 0; i < 10; i++){
+            System.out.println("Nilai akhir ke-"+i+" adalah " + nilaiAkhir[i]);
+        }
+    }
 }
