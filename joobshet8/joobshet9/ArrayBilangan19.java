2fb194673ff89c75088c526ce3ada06045c4c755
Fakri <yusuffakri67@example>
2025-11-12 20:20:03 +0700

    modifikasi percobaan 1

--- joobshet8/joobshet9/ArrayBilangan19.java
+++ joobshet8/joobshet9/ArrayBilangan19.java
@@ -11,9 +11,8 @@ public class ArrayBilangan19 {
         bil [2] = -7;
         bil [3] = 17;
 
-        System.out.println(bil [0]);
-        System.out.println(bil [1]);
-        System.out.println(bil [2]);
-        System.out.println(bil [3]);
+        for (int i = 0; i < 4; i++) {
+            System.out.println(bil [i]);
+        }
     }
 }
