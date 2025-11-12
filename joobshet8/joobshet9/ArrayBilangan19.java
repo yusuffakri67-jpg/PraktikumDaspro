647f2a3fb3ee219891bd59c83c74c742c26f96f9
Fakri <yusuffakri67@example>
2025-11-12 20:13:03 +0700

    Percobaan 1

---
+++ joobshet8/joobshet9/ArrayBilangan19.java
@@ -0,0 +1,19 @@
+package joobshet8.joobshet9;
+import java.util.Scanner;
+public class ArrayBilangan19 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int[] bil = new int [4];
+
+        bil [0] = 5;
+        bil [1] = 13;
+        bil [2] = -7;
+        bil [3] = 17;
+
+        System.out.println(bil [0]);
+        System.out.println(bil [1]);
+        System.out.println(bil [2]);
+        System.out.println(bil [3]);
+    }
+}
